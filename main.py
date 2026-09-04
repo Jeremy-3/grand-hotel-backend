d9b90062d0d1a46b426204e50ed1fb98f76f1991
Jeremy Gitau <jeremyhizashi@gmail.com>
2026-09-04 10:37:25 +0300

    fix: tighten CORS policy by specifying allowed origin

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tighten this to your actual frontend origin(s) before production
+    allow_origins=["https://grand-hotel-chi.vercel.app/"],  # tighten this to your actual frontend origin(s) before production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
